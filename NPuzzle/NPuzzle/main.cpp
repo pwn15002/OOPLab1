6a77ae9fb5d871702bf3044f4b5d097325643cb8
deerel <deerel@gmail.com>
2016-01-28 15:12:03 +0100

    Flyttat switchen

--- NPuzzle/NPuzzle/main.cpp
+++ NPuzzle/NPuzzle/main.cpp
@@ -13,7 +13,29 @@ int main() {
 
 		board.printBoard();
 
-		board.makeMove(_getch());
+		char userInput = _getch();
+
+		switch (userInput)
+		{
+		case 'w':
+		case 'W':
+			board.moveUp();
+			break;
+		case 's':
+		case 'S':
+			board.moveDown();
+			break;
+		case 'a':
+		case 'A':
+			board.moveLeft();
+			break;
+		case 'd':
+		case 'D':
+			board.moveRight();
+			break;
+		default:
+			break;
+		}
 
 		system("cls");
 		if (board.isSorted() == true)
